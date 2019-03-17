8011cfb10fdbdadf018494383f8e608f5c4b19c6
empdierks <16980128+empdierks@users.noreply.github.com>
2019-03-17 15:31:13 -0400

    Took out unnecessary drop boxes

--- Powers.js
+++ Powers.js
@@ -15,31 +15,10 @@ module.exports = function(){
         });
     }
 
+
 /********Drop Boxes***********/
-//Character
-/*Fills in dropdown box for selecting a character*/
-    function getCharDropDown(res, mysql, context, complete){
-        mysql.pool.query("SELECT id, hname FROM Characters", function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.powers  = results;
-            complete();
-        });
-    }
-//Power
-/*Fills in dropdown box for selecting a power*/
-    function getPowerDropDown(res, mysql, context, complete){
-        mysql.pool.query("SELECT id, ability FROM Powers", function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.powers = results;
-            complete();
-        });
-    }
+//don't need any
+
 /*******Routing Actions********/
 //Display Powers Table
     /*Display all Powers & fills each dropdown box. Requires web based javascript to delete users with AJAX*/
@@ -49,23 +28,22 @@ module.exports = function(){
         //context.jsscripts = ["deleteweapon.js"];
         var mysql = req.app.get('mysql');
         getPowerList(res, mysql, context, complete);
-        getCharDropDown(res, mysql, context, complete);
-        getPowerDropDown(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 3){
+            if(callbackCount >= 1){
                 res.render('powers', context);
             }
 
         }
     });
-
-
-//add powers
-router.post('/', function(req, res){
-        var mysql = req.app.get('mysql'); 
-        var sql = "INSERT INTO Powers (ability) VALUES (?)";
-        var inserts = [req.body.ability];
+//Add Power
+  /* Adds a power then redirects to the powers page after adding */
+    router.post('/', function(req, res){
+        var mysql = req.app.get('mysql');
+        if(req.body['new_power']){
+            var sql = "INSERT INTO Powers (ability) VALUES (?)";
+            var inserts = [req.body.ability];
+        }
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
@@ -75,11 +53,7 @@ router.post('/', function(req, res){
                 res.redirect('/powers');
             }
         });
-           
     });
-//add power to weapon --dropdown boxes
 
     return router;
 }();
-
-//Add character to super team -- dropdown boxes
